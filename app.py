c6e379d5183e4131e9cd3c13461d83ed95d9b7c5
Arman <iperov.55@gmail.com>
2025-03-17 02:13:59 +0400

    everything works only the css is unfinished

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def welcome():
 def get_IP():
     data_url = requests.get('https://emvnh9buoh.execute-api.us-east-1.amazonaws.com/getData?device_id=14&start_time=2025-03-15&end_time=2025-03-16')
     data = json.loads(data_url.content)   
-    print(data["keys"])
     return render_template("getIp.html", newdata=data)
 
 if __name__ == '__main__':
